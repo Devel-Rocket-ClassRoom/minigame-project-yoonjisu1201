8f8cc482bc597d4db72542966adb1fba49448c81
윤지수 <yoonjisu1201@gmail.com>
2026-05-21 02:06:50 +0900

    #19 완성 요리 드래서해서 서빙, 서빙로직 정상작동

--- Assets/Scripts/Cooking/DraggableFood.cs
+++ Assets/Scripts/Cooking/DraggableFood.cs
@@ -1,10 +1,12 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 [RequireComponent(typeof(Collider2D))]
 public class DraggableFood : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
-    private RecipeSO _recipe;
+    private RecipeSO _recipe; //조리대 슬롯에서 만든 레시피
+    private CookingSlot _slot;
     public Vector3 _originPosition;
     private Camera _cam;
 
@@ -12,9 +14,10 @@ public class DraggableFood : MonoBehaviour, IBeginDragHandler, IEndDragHandler, 
     {
         _cam = Camera.main;
     }
-    public void Setup(RecipeSO recipe)
+    public void Setup(RecipeSO recipe, CookingSlot slot)
     {
         _recipe = recipe;
+        _slot = slot;
         gameObject.SetActive(true);
     }
     public void OnBeginDrag(PointerEventData eventData)
@@ -22,7 +25,7 @@ public class DraggableFood : MonoBehaviour, IBeginDragHandler, IEndDragHandler, 
         _originPosition = transform.position;
     }
     public void OnDrag(PointerEventData eventData)
-    { 
+    {
         Vector3 worldPos = _cam.ScreenToWorldPoint(eventData.position);
         worldPos.z = transform.position.z;
         transform.position = worldPos;
@@ -30,6 +33,38 @@ public class DraggableFood : MonoBehaviour, IBeginDragHandler, IEndDragHandler, 
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        throw new System.NotImplementedException();
+        if (_recipe == null)
+        {
+            transform.position = _originPosition;
+            return;
+        }
+        Vector2 dropPos = _cam.ScreenToWorldPoint(eventData.position);
+        Collider2D[] hits = Physics2D.OverlapPointAll(dropPos);
+
+        Debug.Log($"드롭 위치: {dropPos}, 감지된 콜라이더 수: {hits.Length}");
+
+        foreach (var hit in hits)
+        {
+            Debug.Log($"hit: {hit.gameObject.name}");
+            if (hit.gameObject == gameObject) continue;
+
+            var guest = hit.GetComponent<GuestController>();
+            Debug.Log($"GuestController 있음: {guest != null}");
+            if (guest == null) continue;
+
+            if (guest.CurrentOrder != _recipe) continue;
+            //_slot.Ingredients는 IReadOnlyList 이기 때문에 복사해서 사용
+            bool isValid = RecipeValidator.ValidateForGuest(
+                new List<IngredientSO>(_slot.Ingredients), guest.CurrentOrder, guest.GhostData);
+
+            if (isValid)
+            {
+                    guest.ReceiveFood();
+                    _slot.CollectAndReset();
+                    gameObject.SetActive(false);
+                    return;
+            }
+        }
+        transform.position = _originPosition;
     }
 }
--- Assets/Scripts/Customer/OrderPopup.cs
+++ Assets/Scripts/Customer/OrderPopup.cs
@@ -5,6 +5,7 @@ public class OrderPopup : MonoBehaviour
     [SerializeField] private SpriteRenderer _orderIcon;
     [SerializeField] private SpriteRenderer _menuIcon;
 
+    //손님 컨트롤러에서 사용
      public void Show(RecipeSO recipe)
     {
         if (recipe == null) return;
